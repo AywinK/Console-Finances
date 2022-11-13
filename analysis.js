226ebe5e4feb90d8d310e0d8c7249e8b42ae7a02
AywinK <115905373+AywinK@users.noreply.github.com>
2022-11-13 20:24:28 +0000

    removed unwanted console.logs and added final analysis summary console.log

--- analysis.js
+++ analysis.js
@@ -2,7 +2,6 @@
 // Finding total number of months
 var total_months = finances.length;
 var total_months_array = "Total months (datapoints) included in dataset: " + total_months;
-console.log(total_months_array);
 
 // Net total amount profit/losses over whole period
 // initialise variables
@@ -13,13 +12,10 @@ for (i = 0; i < finances.length; i++) {
     net_total_money += finances[i][1];
 }
 var net_total_money_array = "Net total of profit/losses over entire period: ($" + net_total_money + ")";
-// console log total money
-console.log(net_total_money_array);
 
 // net profit/loss for whole period averaged out over each month
 var avg_monthly_net = Math.round((net_total_money / total_months) * 100) / 100;
 var avg_monthly_net_array = "Average net total of profit/losses every month over whole period: ($" + avg_monthly_net + ")";
-console.log(avg_monthly_net_array);
 
 // monthly profit/loss from dataset put into variable
 // initialise variables
@@ -30,7 +26,6 @@ for (i = 0; i < finances.length; i++) {
     monthly_money[i] = finances[i][1];
 }
 
-console.log(monthly_money)
 
 // change in monthly_money between months
 // initialise chng_in_monthly_money
@@ -42,8 +37,6 @@ for (i = 1; i < monthly_money.length; i++ ) {
     chng_in_monthly_money[i - 1] = monthly_money[i] - monthly_money[i - 1];
 }
 
-// print chng_in_monthly_money to console
-console.log(chng_in_monthly_money);
 
 // avg change over whole period but given as a per month value - using for loop to meet acceptance criteria maybe
 var ttl_chng_in_monthly_money = 0;
@@ -59,10 +52,8 @@ avg_chng_in_monthly_money = ttl_chng_in_monthly_money/(monthly_money.length-1);
 // appropriate rounding of currency
 avg_chng_in_monthly_money = Math.round(avg_chng_in_monthly_money * 100) / 100;
 // array for console
-var change_in_period_per_month_array = "Overall change in whole period given (given as per month figure): ($" + avg_chng_in_monthly_money + ")";
+var change_in_period_per_month_array = "Overall change in profit over entire period (given as per month figure): ($" + avg_chng_in_monthly_money + ")";
 
-
-console.log(change_in_period_per_month_array)
 
 // avg change (fluctuations) between months
 var monthly_money_fluc = 0;
@@ -74,24 +65,34 @@ var avg_monthly_money_fluc = monthly_money_fluc / (monthly_money.length - 1);
 avg_monthly_money_fluc = Math.round(avg_monthly_money_fluc * 100) / 100;
 var avg_monthly_money_fluc_array = "Average fluctuations in profit between months during whole period (absolute value): ($" + avg_monthly_money_fluc + ")";
 
-console.log(avg_monthly_money_fluc_array)
 
 // Greatest profit (increase in profit)
 var great_profit_inc = Math.max(...chng_in_monthly_money);
-console.log(great_profit_inc);
 // Find the index of greatest profit
 var profit_i = chng_in_monthly_money.indexOf(great_profit_inc) + 1;
-console.log(profit_i);
 // find array with greatest profit in finances data
 var profit_array = "Greatest increase in Profits between months:  " + finances[profit_i][0] + " ($" + great_profit_inc + ")";
-console.log(profit_array);
 
 // Greatest profit (greatest decrease in profit)
 var great_profit_dec = Math.min(...chng_in_monthly_money);
-console.log(great_profit_dec);
 // Find the index of greatest loss
 var loss_i = chng_in_monthly_money.indexOf(great_profit_dec) + 1;
-console.log(loss_i);
 // find array with greatest loss in finances data
 var loss_array = "Greatest decrease in Profits between months:  " + finances[loss_i][0] + " ($" + great_profit_dec + ")";
-console.log(loss_array);
+
+
+// printing analysis to console
+var finances_analysis =
+`Financial Analysis
+---------------------------------
+` + total_months_array + `
+` + net_total_money_array + `
+` + avg_monthly_net_array + `
+` + change_in_period_per_month_array + `
+` + avg_monthly_money_fluc_array + `
+` + profit_array + `
+` + loss_array
+;
+
+
+console.log(finances_analysis)
