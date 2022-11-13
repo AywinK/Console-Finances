a84b12f2b013599983c907e7cb40ed7179b5d264
AywinK <115905373+AywinK@users.noreply.github.com>
2022-11-13 00:42:12 +0000

    average change code now correctly reflects acceptance criteria

--- analysis.js
+++ analysis.js
@@ -22,9 +22,9 @@ for (i = 0; i < finances.length; i++) {
 // console log total money
 console.log(total_money);
 
-// Average of changes month by month over whole period
-var monthly_average = total_money / total_months;
-console.log(monthly_average);
+// // Average of changes month by month over whole period
+// var monthly_average = total_money / total_months;
+// console.log(monthly_average);
 
 // month with greatest increase in profits
 // initialise variables
@@ -36,6 +36,33 @@ for (i = 0; i < finances.length; i++) {
 }
 
 console.log(monthly_money)
+
+// average change in monthly_money
+// initialise chng_in_monthly_money
+var chng_in_monthly_money = [];
+
+// calculate array of monthly change - month to month
+// initialise from second month onwards
+for (i = 1; i < monthly_money.length; i++ ) {
+    chng_in_monthly_money[i - 1] = monthly_money[i] - monthly_money[i - 1];
+}
+
+// print chng_in_monthly_money to console
+console.log(chng_in_monthly_money);
+
+// calculate average (mean value, not median or mode) of change in monthly money
+var avg_chng_in_monthly_money = 0;
+
+for (i = 0; i < chng_in_monthly_money.length; i++) {
+    avg_chng_in_monthly_money += chng_in_monthly_money[i];
+}
+
+console.log(avg_chng_in_monthly_money)
+
+// avg_chng_in_monthly_money = avg_chng_in_monthly_money / avg_chng_in_monthly_money.length;
+
+// print avereage change in monthly money
+console.log(avg_chng_in_monthly_money / (monthly_money.length-1));
 
 // Greatest profit
 var great_profit = Math.max(...monthly_money);
